1aa4b4d71234cea76c799bec68c1b69fcdb4cb7c
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-22 16:27:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,8 +6,3 @@ function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
   return playlist
 }
-
-function removeFromPlaylist(playlist, artist) {
-  delete playlist[artist]
-  return playlist
-}
